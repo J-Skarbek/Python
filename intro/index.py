4d994fa8675cb75dfe69bfd5475f46c0c74c087e
J-Skarbek <john@snarkmediaagency.com>
2024-03-25 16:04:06 -0500

    String exercise 1

--- intro/index.py
+++ intro/index.py
@@ -1,13 +1,22 @@
-print('Basic Arithmetic')
+msg='welcome to Python 101: Strings'
+print(msg, msg)
+print(msg.upper())
+print(msg.lower())
 
+print(msg.capitalize())
+      
+print(msg.title())
 
-a=10
-b=3
-print('Addition : ', a + b)
-print('Subtraction : ', a - b)
-print('Multiplication : ', a * b)
-print('Division (float) : ', a / b)
-print('Division (floor) : ', a // b)
-print('Modulus : ', a % b)
-print('Exponent : ', a ** b)
+print(len(msg)) #measure lenght of string
 
+print(msg.count('Python')) #this command is case sensitive
+
+string_to_slice = 'We\'re going to be splicing this string up!'
+
+print(string_to_slice[-1])
+
+welcome_statement = msg[18] + ' ' + msg[0:7] + ' Ring to Tyler'
+print(welcome_statement.title())
+
+reverse_statement = welcome_statement[::-1]
+print(reverse_statement)
