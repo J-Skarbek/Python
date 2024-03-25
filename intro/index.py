8b301967fb022328257c7e1dfceb64f117606eed
J-Skarbek <john@snarkmediaagency.com>
2024-03-25 10:27:37 -0500

    Create some test variables

--- intro/index.py
+++ intro/index.py
@@ -1,2 +1,8 @@
 print('Hello world')
 print(45)
+
+test_variable="6"
+abas="another variable"
+
+print('This string displays the 1st test variable: ' + test_variable + '....')
+print('here\'s a second one' + abas)
