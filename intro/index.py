85dd91939e0b004d30c6af03ad82a70de7803ac1
J-Skarbek <john@snarkmediaagency.com>
2024-03-25 16:17:06 -0500

    String exercise 2

--- intro/index.py
+++ intro/index.py
@@ -20,3 +20,27 @@ print(welcome_statement.title())
 
 reverse_statement = welcome_statement[::-1]
 print(reverse_statement)
+
+multiline = """Hello,
+this is a mulitline string
+and it requires the triple-quotes!"""
+
+print(multiline)
+
+print(msg)
+
+replaced_msg = msg.replace('Python', 'DingDongs')
+
+print(replaced_msg)
+
+print('Python' in msg) #will check for the string within the msg string
+
+print('Python' not in msg) #will check for the string to not exist within the msg string
+
+
+name='TERRY'
+color = 'RED'
+example1 = '[' + name + '] loves the color ' + color + '!'
+example2 = f'[{name}] loves the color {color.lower()}!'
+print(example1)
+print(example2)
