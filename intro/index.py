a2aa0d8b10d3af620f3b345599bb69c086941bc1
J-Skarbek <john@snarkmediaagency.com>
2024-03-27 09:43:36 -0500

    User input exercise 2

--- intro/index.py
+++ intro/index.py
@@ -1,52 +1,7 @@
-msg='welcome to Python 101: Strings'
-print(msg, msg)
-print(msg.upper())
-print(msg.lower())
+first_name = input('What\'s your first name?')
+dist_in_kilometers = input('How far away are you?')
 
-print(msg.capitalize())
-      
-print(msg.title())
+converted = float(dist_in_kilometers) / 1.609
 
-print(len(msg)) #measure lenght of string
-
-print(msg.count('Python')) #this command is case sensitive
-
-string_to_slice = 'We\'re going to be splicing this string up!'
-
-print(string_to_slice[-1])
-
-welcome_statement = msg[18] + ' ' + msg[0:7] + ' Ring to Tyler'
-print(welcome_statement.title())
-
-reverse_statement = welcome_statement[::-1]
-print(reverse_statement)
-
-multiline = """Hello,
-this is a mulitline string
-and it requires the triple-quotes!"""
-
-print(multiline)
-
-print(msg)
-
-replaced_msg = msg.replace('Python', 'DingDongs')
-
-print(replaced_msg)
-
-print('Python' in msg) #will check for the string within the msg string
-
-print('Python' not in msg) #will check for the string to not exist within the msg string
-
-
-name='TERRY'
-color = 'RED'
-example1 = '[' + name + '] loves the color ' + color + '!'
-example2 = f'[{name}] loves the color {color.lower()}!' #works like backticks in JS -- apparently needs an 'f' at beginning to work
-print(example1)
-print(example2)
-
-name = input('What is your name?')
-print('My name is ' + name)
-
-age = input('What is your age?')
-print('I am ' + age + ' years old.')
+greeting = 'Nice to see you ' + first_name.capitalize() + ', you are ' + str(round(converted, 1)) + ' miles from me.'
+print(greeting)
