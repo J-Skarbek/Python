127fb73f55efcded45b1c128704f8e57f951ac7f
J-Skarbek <john@snarkmediaagency.com>
2024-03-27 09:50:34 -0500

    Lists

--- intro/index.py
+++ intro/index.py
@@ -1,7 +1,15 @@
-first_name = input('What\'s your first name?')
-dist_in_kilometers = input('How far away are you?')
+# first_name = input('What\'s your first name?')
+# dist_in_kilometers = input('How far away are you?')
 
-converted = float(dist_in_kilometers) / 1.609
+# converted = float(dist_in_kilometers) / 1.609
 
-greeting = 'Nice to see you ' + first_name.capitalize() + ', you are ' + str(round(converted, 1)) + ' miles from me.'
-print(greeting)
+# greeting = 'Nice to see you ' + first_name.capitalize() + ', you are ' + str(round(converted, 1)) + ' miles from me.'
+# print(greeting)
+
+murray_family = ['Thad', 'Linda', 'Justice', 'TJ']
+
+print(murray_family[1])
+print(murray_family[:2])
+
+print(len(murray_family))
+print(murray_family.count('Linda'))
