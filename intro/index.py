d90befdf40f4121a953c27851f37ca0147ead249
J-Skarbek <john@snarkmediaagency.com>
2024-03-28 16:35:43 -0500

    Tuples stuff

--- intro/index.py
+++ intro/index.py
@@ -12,15 +12,22 @@
 # print(msg.replace(' ', ''))
 
 
-csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
-friends_list = ['Exercise: fill me with names']
-print(friends_list)
+# csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
+# friends_list = ['Exercise: fill me with names']
+# print(friends_list)
 
-csv = csv.replace(':', ',')
-csv = csv.replace(';', ',')
-print(csv.split(','), type (csv.split(',')))
+# csv = csv.replace(':', ',')
+# csv = csv.replace(';', ',')
+# print(csv.split(','), type (csv.split(',')))
 
-#can also chain the functions like this:
+# #can also chain the functions like this:
 
-print(csv.replace(';', ',').replace(':', ',').split(','))
+# print(csv.replace(';', ',').replace(':', ',').split(','))
 
+########
+
+#Tuples - faster Lists you can't change
+friends = ['John','Michael','Terry','Eric','Graham']
+friends_tuple = ('John','Michael','Terry','Eric','Graham')
+print(friends[2])
+print(friends_tuple[2:3])
